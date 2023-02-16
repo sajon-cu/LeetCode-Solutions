8c3bafc9347551a9ea4d633f4ddaac180ea22a1f
Saiful Islam Sajon <32152920+sajon-cu@users.noreply.github.com>
2023-02-16 19:42:39 +0600

    Time: 192 ms (39.67%), Space: 35.6 MB (82.67%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.kt
@@ -0,0 +1,20 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+        if(root == null) return 0
+        
+        val left = maxDepth(root.left)
+        val right = maxDepth(root.right)
+        
+        return Math.max(left, right) + 1
+    }
+}
