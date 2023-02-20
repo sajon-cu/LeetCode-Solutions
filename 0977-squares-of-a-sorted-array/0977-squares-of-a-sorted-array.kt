08e9b394ea25e9d8a927fe3ea6bffe14a45c3b19
Saiful Islam Sajon <32152920+sajon-cu@users.noreply.github.com>
2023-02-20 11:47:17 +0600

    Time: 329 ms (67.42%), Space: 40.8 MB (67.87%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.kt
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.kt
@@ -1,10 +1,28 @@
 class Solution {
     fun sortedSquares(nums: IntArray): IntArray {
-        val res = nums.map { num ->
-            num*num
-        }.toIntArray()
+//         val res = nums.map { num ->
+//             num*num
+//         }.toIntArray()
         
-        res.sort()
+//         res.sort()
+//         return res
+        
+        val size = nums.size
+        val res = IntArray(size)
+        var i = 0
+        var j = size - 1
+        
+        for (ii in size - 1 downTo 0) {
+            if(Math.abs(nums[i]) > Math.abs(nums[j])) {
+                res[ii] = nums[i] * nums[i]
+                i++
+            } else {
+                res[ii] = nums[j] * nums[j]
+                j--
+            }
+        }
+        
         return res
     }
 }
+
