72cf4356025fb7996718a7199501122e4b11090d
Saiful Islam Sajon <32152920+sajon-cu@users.noreply.github.com>
2023-02-21 13:44:17 +0600

    Time: 283 ms (69.44%), Space: 44.6 MB (69.44%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun singleNonDuplicate(nums: IntArray): Int {
+        val size = nums.size
+        
+        if(size == 1) return nums[0]
+        
+        var i = 0
+        var res: Int = 0
+        
+        while(i < size - 1) {
+            if(nums[i] == nums[i+1]) {
+                i += 2
+            } else {
+                res = nums[i]
+                break
+            }
+        }
+        
+        return if(res==0) nums[i] else res    
+    }
+}
