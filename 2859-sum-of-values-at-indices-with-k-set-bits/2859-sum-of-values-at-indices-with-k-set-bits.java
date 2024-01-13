787dd36ee3b9b0e18fe8c2af811dd834217327a7
Saiful Islam Sajon <32152920+sajon-cu@users.noreply.github.com>
2024-01-13 21:13:45 +0600

    Time: 78 ms (5.21%), Space: 45.4 MB (6.66%) - LeetHub

---
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+        int sum = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            String binary = decimalToBinary(i);
+            if(isValidLengthOf(binary, k)) {
+                sum += nums.get(i);
+            }
+        }
+
+        return sum;
+    }
+
+
+    boolean isValidLengthOf(String binary, int k) {
+        int count = 0;
+        for(int j = 0; j < binary.length(); j++) {
+            if(binary.charAt(j) == '1') {
+                count++;
+            }
+        }
+
+        return count == k ;
+    }
+
+    public String decimalToBinary(int num) {
+        StringBuilder sb = new StringBuilder();
+
+        if(num == 0) {
+            System.out.println(num);
+            sb.append(0);
+        }
+
+        while(num != 0) {
+            int rem = num % 2;
+            num /= 2;
+            sb.append(rem);
+        }
+
+        System.out.print(sb +" ");
+
+        return sb.toString();
+    }
+}
