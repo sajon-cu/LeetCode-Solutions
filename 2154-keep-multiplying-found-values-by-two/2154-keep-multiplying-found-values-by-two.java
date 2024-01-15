fb53da9551e43acfe80ae7b774d56bcf4d4ae7e6
Saiful Islam Sajon <32152920+sajon-cu@users.noreply.github.com>
2024-01-15 23:35:04 +0600

    Time: 3 ms (60.24%), Space: 44.3 MB (15.97%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
@@ -1,13 +1,14 @@
 class Solution {
     public int findFinalValue(int[] nums, int original) {
-        Arrays.sort(nums);
-        
-        for(int i : nums) {
-            if(i == original) {
-                original *= 2; 
-            }
-        }
-        
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : nums)
+            if(i >= original)
+                set.add(i);
+        while(true)
+            if(set.contains(original))
+                original *= 2;
+            else
+                break;
         return original;
     }
 }
